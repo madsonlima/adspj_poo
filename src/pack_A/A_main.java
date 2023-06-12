11b55d7584bfa6383431345a74ef847b4f4faafa
Madson Lima <mad.lima@outlook.com>
2023-06-12 20:44:28 +0000

    Implementação do menu

--- src/pack_A/A_main.java
+++ src/pack_A/A_main.java
@@ -22,19 +22,51 @@ public class A_main {
 		// Ler o csv e armazenar em um araylist --------------------------------------------------------------------------------------------------------------------------------
 		arrayCadastros = Metodos.arqLerArray();
 
-		// Mostrar tabela de cadastro ------------------------------------------------------------------------------------------------------------------------------------------
-		Metodos.arqMostrarArray(arrayCadastros);
+		while (true) {
 
-		// Inserir dados no array ----------------------------------------------------------------------------------------------------------------------------------------------
-		ModeloCadastro novoCadastro = Metodos.arqInserirCadastro(arrayCadastros.get(0));	// Retorna um ModeloCadastro para inserir em novoCadastro. arrayCadastros.get(0) é
-		arrayCadastros.add(novoCadastro);													// recebido para verificar os títulos das colunas.
-		System.out.println(Metodos.ANSI_CYAN + "[SUCESSO] CADASTRO INCLUÍDO: '" + novoCadastro.getNome() + "'\n" + Metodos.ANSI_RESET);
+			String opcao = "";
+			while ((opcao != 1)||(opcao != 2)||(opcao != 3)||(opcao != 4)||(opcao != 5)){
 
-		// Alterar dados de cadastros ------------------------------------------------------------------------------------------------------------------------------------------
-		arrayCadastros = Metodos.arqAlterarCadastro(arrayCadastros);
+				opcao = System.out.println("Digite a Opção:\n1- Mostrar Cadastros.\n2- Inserir Cadastro.\n3- Alterar Cadastro.\n4- Deletar Cadastro.\n5- Salvar e Sair.\n");
 
-		// Deletar cadastro ----------------------------------------------------------------------------------------------------------------------------------------------------
-		arrayCadastros = Metodos.arqDeletarCadastro(arrayCadastros);
+			}
+
+            switch (opcao) {
+                case "1":
+					// Mostrar tabela de cadastro -------------------------------------------------------------------------------------------------------------------------------
+                    Metodos.arqMostrarArray(arrayCadastros);
+                    break;
+
+                case "2":
+					// Inserir dados no array -----------------------------------------------------------------------------------------------------------------------------------
+					ModeloCadastro novoCadastro = Metodos.arqInserirCadastro(arrayCadastros.get(0));	// Retorna um ModeloCadastro para inserir em novoCadastro. arrayCadastros.get(0) é
+					arrayCadastros.add(novoCadastro);													// recebido para verificar os títulos das colunas.
+					System.out.println(Metodos.ANSI_CYAN + "[SUCESSO] CADASTRO INCLUÍDO: '" + novoCadastro.getNome() + "'\n" + Metodos.ANSI_RESET);
+                    break;
+
+                case "3":
+					// Alterar dados de cadastros -------------------------------------------------------------------------------------------------------------------------------
+                    arrayCadastros = Metodos.arqAlterarCadastro(arrayCadastros);
+                    break;
+
+                case "4":
+					// Deletar cadastro -----------------------------------------------------------------------------------------------------------------------------------------
+                    arrayCadastros = Metodos.arqDeletarCadastro(arrayCadastros);
+                    break;
+
+                case "5":
+                    System.out.println("SALVANDO E ENCERRANDO!");
+                    break;
+
+            }
+
+			if (opcao == 5) {
+
+				break;
+
+			}
+
+        }
 
 		// Sobrescrever arquivo csv --------------------------------------------------------------------------------------------------------------------------------------------
 		Metodos.arqSobrescrever(arrayCadastros);
